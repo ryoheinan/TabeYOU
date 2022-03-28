ea23434c7bb675a443a76e1b6a2278823b523508
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-28 11:41:59 +0900

    feat: Create header component

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="text-center mt-2">
+    <h1>TabeYOU</h1>
+    <p class="mb-0">あなたのための飲食店検索サイト</p>
+    <p class="small text-muted">
+      Powered by
+      <a href="http://webservice.recruit.co.jp/" target="_blank" rel="noopener noreferrer"
+        >ホットペッパー Webサービス</a
+      >
+    </p>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 700;
+  margin-bottom: 0.1rem;
+}
+</style>
