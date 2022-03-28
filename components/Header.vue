b276514f69b350d7034e2718aa56c13d2212b352
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-28 21:02:00 +0900

    chore: Update header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,18 @@
 <template>
-  <header class="text-center mt-2">
+  <header class="text-center py-2">
     <h1>TabeYOU</h1>
     <p class="mb-0">あなたのための飲食店検索サイト</p>
-    <p class="small text-muted">
-      Powered by
-      <a href="http://webservice.recruit.co.jp/" target="_blank" rel="noopener noreferrer"
-        >ホットペッパー Webサービス</a
-      >
-    </p>
   </header>
 </template>
 
 <style scoped>
+header {
+  background-color: #f3ae56;
+  text-align: center;
+  padding: 1rem 0;
+  margin-bottom: 2rem;
+}
+
 h1 {
   font-weight: 700;
   margin-bottom: 0.1rem;
