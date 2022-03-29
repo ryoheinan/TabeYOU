bba5183ba70a7d5d019458ea8534096b6e86290b
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-29 21:08:39 +0900

    feat: Create error page

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header />
+    <main class="text-center" role="main">
+      <div class="msg-area">
+        <div>
+          <h1 class="text-center text-danger mb-3">
+            {{ error.statusCode }} エラー!
+          </h1>
+          <NuxtLink class="page-link d-inline-block" to="/" role="button"
+            >TOPへ</NuxtLink
+          >
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  head() {
+    return {
+      title: `${this.error.statusCode}エラー`,
+    }
+  },
+}
+</script>
