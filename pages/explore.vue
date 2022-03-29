39f629122090cd0220ff4c7956e7b3cb59228680
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-29 13:10:35 +0900

    feat: Create explore page

--- pages/explore.vue
+++ pages/explore.vue
@@ -1,7 +1,58 @@
 <template>
   <div>
-    <h1>Explore</h1>
-    {{ restaurants.data }}
+    <Header />
+    <main class="container d-flex justify-content-center">
+      <div v-if="restaurants.errorMsg" class="msg-area">
+        <div>
+          <div class="h5 text-center text-danger">エラー!</div>
+          {{ restaurants.errorMsg }}
+        </div>
+      </div>
+      <div v-else-if="restaurants.isLoading" class="msg-area">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+      <div v-else>
+        <h1 class="h2">検索一覧</h1>
+        <p class="small text-muted">
+          {{ restaurants.resultsAvailable }}件見つかりました。
+        </p>
+        <nuxt-link
+          v-for="shop in restaurants.shops"
+          :key="shop.id"
+          :to="{ name: 'shops-id', params: { id: shop.id } }"
+          class="card-link"
+        >
+          <div class="card mb-3">
+            <div class="row g-0">
+              <div
+                class="col-md-2 d-flex justify-content-center align-items-center py-2 py-md-0"
+              >
+                <img
+                  :src="shop.logo_image"
+                  :alt="
+                    shop.logo_image.includes('noimage')
+                      ? 'ロゴ画像提供なし'
+                      : `${shop.name}のアイコン画像`
+                  "
+                />
+              </div>
+              <div class="col-md-10">
+                <div class="info">
+                  <p class="h4 card-title">{{ shop.name }}</p>
+                  <p>{{ shop.catch }}</p>
+                  <dl>
+                    <dt>アクセス</dt>
+                    <dd>{{ shop.access }}</dd>
+                  </dl>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -19,7 +70,9 @@ export default defineComponent({
     const { $axios, query } = useContext()
 
     const restaurants = reactive({
-      data: {},
+      shops: {},
+      resultsAvailable: 0,
+      resultsPerPage: 0,
       isLoading: true,
       errorMsg: '',
     })
@@ -55,7 +108,9 @@ export default defineComponent({
         if (response.results.error) {
           restaurants.errorMsg = response.results.error[0].message
         } else {
-          restaurants.data = response.results
+          restaurants.shops = response.results.shop
+          restaurants.resultsAvailable = response.results.results_available
+          restaurants.resultsPerPage = response.results.results_returned
         }
       } catch (error) {
         restaurants.errorMsg = 'Failed to get gourmet data.'
@@ -69,4 +124,39 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  box-shadow: 0 0.2rem 0.4rem rgb(0 0 0 / 14%);
+  transition: ease-in-out 0.25s;
+}
+
+.card:hover {
+  transform: scale(1.025, 1.025);
+}
+
+.card-link {
+  text-decoration: none;
+  color: #000000;
+}
+
+.card-link:hover {
+  text-decoration: none;
+  color: #000000;
+}
+
+.info {
+  margin: 1.25rem 0.5rem 1.25rem 0;
+}
+
+@media (max-width: 768px) {
+  .info {
+    margin: 1.25rem 1rem;
+  }
+}
+
+@media (min-width: 768px) {
+  .card {
+    width: 50rem;
+  }
+}
+</style>
